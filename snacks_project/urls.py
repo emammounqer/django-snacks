4b4a81a2646b48ab60beac439bccd5cda4d68c8c
emammounqer <emam.anagreh@gmail.com>
2023-06-11 17:44:35 +0300

    add snake app

--- snacks_project/urls.py
+++ snacks_project/urls.py
@@ -16,7 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.urls.conf import include
+from snacks.urls import urlpatterns as snacks_urls
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include(snacks_urls), name='snacks'),
 ]
