0f7cec709f15e0ad6bafddf38d7db015e1477c31
emammounqer <emam.anagreh@gmail.com>
2023-06-12 16:41:34 +0300

    update page title

--- snacks/views.py
+++ snacks/views.py
@@ -1,3 +1,4 @@
+from typing import Any, Dict
 from django.views.generic import TemplateView
 
 # Create your views here.
@@ -6,6 +7,16 @@ from django.views.generic import TemplateView
 class HomeView(TemplateView):
     template_name = 'home.html'
 
+    def get_context_data(self, **kwargs: Any) -> Dict[str, Any]:
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'Home Page'
+        return context
+
 
 class AboutView(TemplateView):
     template_name = 'about.html'
+
+    def get_context_data(self, **kwargs: Any) -> Dict[str, Any]:
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'About Page'
+        return context
